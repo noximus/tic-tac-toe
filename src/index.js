be47a1ef8be46df189cb7d33cf8f14183a2da119
Henry Richardson <hrichardson83@gmail.com>
2019-01-31 16:00:47 -0500

    click added to button

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class Square extends React.Component {
   render() {
     return (
       <div>
-        <button className="square">
+        <button className="square" onClick={ () => alert('click') }>
           {this.props.value}
         </button>
       </div>
