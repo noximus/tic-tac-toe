a7acbc0a1b56e32e12527789ebac31e7a1c482fb
Henry Richardson <hrichardson83@gmail.com>
2019-02-01 12:55:30 -0500

    end game if winner or check to see if square is filled

--- src/index.js
+++ src/index.js
@@ -23,6 +23,9 @@ class Board extends React.Component {
   
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -40,7 +43,6 @@ class Board extends React.Component {
   }
 
   render() {
-    // const status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
     const winner = calculateWinner(this.state.squares);
     let status;
     if (winner) {
