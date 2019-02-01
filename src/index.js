5a2268bcfed915d94f1dcbad0dd5e59d41e1ed7b
Henry Richardson <hrichardson83@gmail.com>
2019-02-01 12:39:56 -0500

    xIsNext to change whose turn it is

--- src/index.js
+++ src/index.js
@@ -2,20 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// class Square extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         <button 
-//           className="square" 
-//           onClick={ () => this.props.onClick() }
-//         >
-//           {this.props.value}
-//         </button>
-//       </div>
-//     );
-//   }
-// }
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -24,22 +10,23 @@ function Square(props) {
   )
 }
 
-export default index
-
-
 class Board extends React.Component {
   constructor(props) {
     super(props)
   
     this.state = {
        squares: Array(9).fill(null),
+       xIsNext: true,
     }
   }
   
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares})
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    })
   }
 
   renderSquare(i) {
@@ -52,7 +39,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next Player: X';
+    const status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
