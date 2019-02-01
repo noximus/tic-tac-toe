cc79e8b10c0fe17db80ec1692baa9386d7d74cdd
Henry Richardson <hrichardson83@gmail.com>
2019-02-01 12:36:46 -0500

    converted component to function component

--- src/index.js
+++ src/index.js
@@ -2,20 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <div>
-        <button 
-          className="square" 
-          onClick={ () => this.props.onClick() }
-        >
-          {this.props.value}
-        </button>
-      </div>
-    );
-  }
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <button 
+//           className="square" 
+//           onClick={ () => this.props.onClick() }
+//         >
+//           {this.props.value}
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
+
+export default index
+
 
 class Board extends React.Component {
   constructor(props) {
